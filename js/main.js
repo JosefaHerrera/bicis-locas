9c25cfcca5cf1a5bdb753a598ac8880520a5079b
JosefaHerrera <josefa@linuxero.cl>
2017-05-15 14:46:45 -0400

    validacion con console.log, ahora probar con span

--- js/main.js
+++ js/main.js
@@ -6,15 +6,15 @@ function validateForm(){
 		var nombre = document.getElementById('name').value; //valor ingresado al input en el id='name'
 		     //validar que nombre no esté vacío
 		if(nombre === "" || nombre === undefined || nombre.length === 0){
-			alert("Error, debe ingresar nombre");
+			console.log("Error, debe ingresar nombre");
 			//validar que 1° caracter sea mayúscula
 		}else if(nombre.charAt(0) != nombre.charAt(0).toUpperCase()){
-			alert("Error, el nombre debe comenzar con mayúscula");
+			console.log("Error, el nombre debe comenzar con mayúscula");
 			//validar que sean solo letras
 		}else if(/[0-9]/.test(nombre)){
-			alert("Error, no debe ingresar números")
+			console.log("Error, no debe ingresar números")
 		}else{
-			alert("Nombre ingresado correctamente")
+			console.log("Nombre ingresado correctamente")
 		}
 	}
 	nombre();
@@ -23,13 +23,13 @@ function validateForm(){
 		var Apellido = document.getElementById('lastname').value; 
 			//validar que Apellido no esté vacío
 		if(Apellido === "" || Apellido === undefined || Apellido.length === 0){
-			alert("Error, debe ingresar Apellido");
+			console.log("Error, debe ingresar Apellido");
 			//validar que 1° caracter sea mayúscula
 		}else if(Apellido.charAt(0) != Apellido.charAt(0).toUpperCase()){
-			alert("Error, el Apellido debe comenzar con mayúscula");
+			console.log("Error, el Apellido debe comenzar con mayúscula");
 			//validar que sean solo letras
 		}else if(/[0-9]/.test(Apellido)){
-			alert("Error, no debe ingresar números")
+			console.log("Error, no debe ingresar números")
 		}else{
 			console.log("Apellido ingresado correctamente")
 		}
@@ -41,15 +41,15 @@ function validateForm(){
 		var contrasenia = document.getElementById('input-password').value;
 			//no vacio
 		if(contrasenia === "" || contrasenia === undefined || contrasenia.length === 0){
-			alert("Error, debe ingresar contraseña");
+			console.log("Error, debe ingresar contraseña");
 			//que no sea 1234 ni "password"
 		}else if(contrasenia === "1234" || contrasenia === "password"){
-			alert("Error, la contraseña debe ser distinta a 1234 o password");
+			console.log("Error, la contraseña debe ser distinta a 1234 o password");
 			//que tenga al menos 6 caracteres
 		}else if(contrasenia.length < 6){
 			console.log("Error, la contraseña debe tener al menos 6 caracteres");
 		}else{
-			alert("Contraseña ingresada correctamente");
+			console.log("Contraseña ingresada correctamente");
 		}
 	}
 	password();
@@ -60,7 +60,7 @@ function validateForm(){
 		//var seleccion = document.getElementsByClassName('form-control')[2]; //hay más de una class con el mismo nombre, entonces necesito la etiqueta en la posición 2, //con ctrl+f puedo buscar
 		for(var i = 0; i < seleccion.length; i++){ //se usa for porque hay que recorrer todas las opciones del select
 			if(seleccion[i].value == "0"){	
-				alert("Debe seleccionar una opción");
+				console.log("Debe seleccionar una opción");
 			}
 		}
 	}
@@ -73,7 +73,7 @@ function validateForm(){
 	    if (expRegCorreo.test(correo)) {
 	        console.log('correo valido');
 	    } else {
-	        alert('correo no valido');
+	        console.log('correo no valido');
 	    }
 	};
 	email();
